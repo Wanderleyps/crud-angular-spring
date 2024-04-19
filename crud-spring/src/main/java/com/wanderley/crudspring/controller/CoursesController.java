0f2ad545dfa50cb505cf020a89812f9c99c708ca
Wanderleyps <wanderleyps794@gmail.com>
2024-04-19 17:25:42 -0300

    add findById in api

--- crud-spring/src/main/java/com/wanderley/crudspring/controller/CoursesController.java
+++ crud-spring/src/main/java/com/wanderley/crudspring/controller/CoursesController.java
@@ -3,7 +3,9 @@ package com.wanderley.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,14 @@ public class CoursesController {
     @GetMapping
     public List<Course> list() {
         return this.courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable long id) {
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+
     }
 
     @PostMapping
