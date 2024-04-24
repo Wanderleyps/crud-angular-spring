baac14ec50df27ac06ffe82ceb1400714c1e5358
Wanderleyps <wanderleyps794@gmail.com>
2024-04-23 21:18:53 -0300

    add softdelete in back

--- crud-spring/src/main/java/com/wanderley/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/wanderley/crudspring/model/Course.java
@@ -1,19 +1,23 @@
 package com.wanderley.crudspring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import io.micrometer.common.lang.NonNull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id=?")
+@SQLRestriction("status <> 'Inativo'")// apenas registros com o campo status diferente de 'Inativo' serão retornados
 @Entity
 public class Course {
 
@@ -23,14 +27,19 @@ public class Course {
     private long id;
 
     @NotBlank
-    @NonNull
+    @NotNull
     @Length(min = 3, max = 100)
     @Column(length = 100, nullable = false)
     private String name;
 
-    @NonNull
+    @NotNull
     @Length(max = 30)
     @Column(length = 30, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 30)
+    @Column(length = 30, nullable = false)
+    private String status = "Ativo";
 
 }
