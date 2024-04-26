dfaed71496ac8c02c27564e776c2ddc97cff2014
Wanderleyps <wanderleyps794@gmail.com>
2024-04-26 16:30:50 -0300

    add solution of enum validation

--- crud-spring/src/main/java/com/wanderley/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/wanderley/crudspring/dto/CourseDTO.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-//import com.wanderley.crudspring.enums.Category;
-//import com.wanderley.crudspring.enums.validation.ValueOfEnum;
+import com.wanderley.crudspring.enums.Category;
+import com.wanderley.crudspring.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -18,9 +18,7 @@ public record CourseDTO(
 
         @NotBlank @NotNull @Length(min = 3, max = 100) String name,
 
-       // @NotNull @Length(max = 30) @ValueOfEnum(enumClass = Category.class)  String category,
-
-        @NotNull @Length(max = 30) String category,
+        @NotNull @Length(max = 30) @ValueOfEnum(enumClass = Category.class)  String category,
         
         @NotNull @NotEmpty @Valid List<LessonDTO> lessons) {
 }
